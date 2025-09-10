72df6fc4576800e88fa1d5d5e354309ac3c189f0
Ryan Tauro <23c35.rayan@sjec.ac.in>
2025-09-10 23:15:34 +0530

    can send and receive messages

--- front/src/ChatRoom.jsx
+++ front/src/ChatRoom.jsx
@@ -6,26 +6,50 @@ function ChatRoom(){
     const { roomcode } = useParams()
     const socket = useRef(null)
 
+    const [ currentMessage, setCurrentMessage ] = useState('')
+    const [ messages, setMessages ] = useState([])
+
     useEffect(()=>{
         socket.current = io("http://localhost:3000")
 
-        console.log(roomcode)
+        socket.current.emit('join_room', {"code":roomcode})
 
-        socket.current.emit('join-room', {"code":roomcode})
-        console.log("joined "+roomcode)
-        socket.current.on('msg', (data)=>{
-        console.log(data)
+        socket.current.on('message', (data)=>{
+            setMessages(prevMes => {
+                return [ ...prevMes, data.message ]
+            })
         })
 
         return ()=>{
         if(!socket.current) socket.current.disconnect()
         }
-    }, [])
+    }, [roomcode])
+
+
+    const messageInput = (e)=>{
+        e.preventDefault()
+        socket.current.emit('message', {"message":currentMessage})
+        setCurrentMessage('')
+    }
 
     return (
         <>
-        <h1>Poop</h1>
-        <h2>More Poop</h2>
+        <div id="roomcode">Room Code: {roomcode}</div>
+        <div id="messages">
+            <h4>Messages</h4>
+            {messages.map((message, index)=>(
+                <div>{message}</div>
+            ))}
+        </div>
+        <form onSubmit={messageInput}>
+            <input 
+            value={currentMessage}
+            type="text"
+            placeholder="Message"
+            onChange={(e)=>setCurrentMessage(e.target.value)}
+            />
+            <button>Send</button>
+        </form>
         </>
     )
 }
