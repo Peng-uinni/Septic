a0cc330b582194970c1f46c1659577c150e464bc
shayan-salian <shayansalian2208@gmail.com>
2025-09-11 21:28:38 +0530

    second

--- sptic/src/App.jsx
+++ sptic/src/App.jsx
@@ -1,46 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Index from './Index.jsx'
+import JoinRoom from './JoinRoom.jsx'
 
-import React from 'react';
-import LightRays from './lightRays';
+import './App.css'
 
-const App = () => {
-  return (
-   <div className="w-full h-screen relative bg-black flex justify-center items-center">
-  <div style={{ width: "100%", height: "600px", position: "relative" }}>
-    <LightRays
-      raysOrigin="top-center"
-      raysColor="#ffffffff"
-      raysSpeed={1.8}
-      lightSpread={0.1}
-      rayLength={2}
-      followMouse={true}
-      mouseInfluence={0.6}
-      noiseAmount={0.4}
-      distortion={0.05}
-      className="custom-rays"
-    />
-  </div>
-
-  <div className="absolute flex flex-col items-center">
-    <h1 className="text-white font-bold text-6xl mb-10">
-      Stand out from the crowd !
-    </h1>
-
-    {/* Buttons row */}
-    <div className="flex w-full justify-between max-w-lg">
-      {/* Left button */}
-      <button className="bg-white text-black px-8 py-6 rounded-lg hover:bg-gray-200">
-        Create Room
-      </button>
-
-      {/* Right button */}
-      <button className="bg-white text-black px-8 py-6 rounded-lg hover:bg-gray-200">
-        Join Room
-      </button>
-    </div>
-  </div>
-</div>
-
-  );
+function App() {
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Index></Index>}></Route>
+      <Route path='/join' element={<JoinRoom></JoinRoom>}></Route>
+     
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
